552c042dd5bbc9dd704e8e7ba6f16a7947cdc6e5
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2025-09-17 16:15:50 +0200

    complete download function

--- R/download.R
+++ R/download.R
@@ -2,10 +2,13 @@ biocstickers_readme_url <- "https://raw.githubusercontent.com/Bioconductor/BiocS
 
 download_biocstickers <- function(
   url = biocstickers_readme_url,
-	where = "originals"
+	where = "originals",
+  overwrite = FALSE
 ) {
 	biocstickers_readme <- readLines(biocstickers_readme_url)
 	png_urls <- .get_urls(biocstickers_readme)
+	dir.create(where, showWarnings = FALSE, recursive = TRUE)
+	.download_stickers(urls, where, overwrite)
 }
 
 ## get the full URL to each sticker
@@ -16,4 +19,21 @@ download_biocstickers <- function(
   x <- gsub("\".*", "", x)
   x <- paste0("https://raw.githubusercontent.com/Bioconductor/BiocStickers/master/", x)
   x
+}
+
+.download_stickers <- function(urls, where, overwrite) {
+  for (url in urls) {
+    filename <- basename(url)
+    destfile <- file.path(where, filename)
+    if (!file.exists(destfile)) {
+      tryCatch({
+        message(paste("Downloading", url))
+        httr::GET(url, httr::write_disk(destfile, overwrite = overwrite))
+      }, error = function(e) {
+        message(paste("Failed to download", url, ":", e$message))
+      })
+    } else {
+      # message(paste("Skipping", filename))
+    }
+  }
 }
