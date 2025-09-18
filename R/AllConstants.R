e58e57c1106c1c1af523c12d17480eb7ee7103ec
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2025-09-18 22:40:33 +0200

    Update AllConstants.R

--- R/AllConstants.R
+++ R/AllConstants.R
@@ -1,3 +1,10 @@
-.biocstickers_readme_url <- "https://raw.githubusercontent.com/Bioconductor/BiocStickers/master/README.md"
+.biocstickers_readme_url <- file.path(
+  "https:/",
+  "raw.githubusercontent.com",
+  "Bioconductor",
+  "BiocStickers",
+  "master",
+  "README.md"
+)
 
 .subdir_originals <- "originals"
