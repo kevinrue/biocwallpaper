1812eda379b89978a8b7f88d4e2001a72eb5f26a
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2025-09-18 11:37:11 +0200

    document

---
+++ R/AllConstants.R
@@ -0,0 +1,3 @@
+.biocstickers_readme_url <- "https://raw.githubusercontent.com/Bioconductor/BiocStickers/master/README.md"
+
+.subdir_originals <- "originals"
--- R/download.R
+++ R/download.R
@@ -1,14 +1,29 @@
-biocstickers_readme_url <- "https://raw.githubusercontent.com/Bioconductor/BiocStickers/master/README.md"
-
+#' Download BiocStickers
+#'
+#' @param where Root directory to download stickers.
+#' The stickers will be downloaded in a subdirectory 'originals' within that directory.
+#' Defaults to current directory.
+#' @param overwrite If `FALSE`, skip stickers already present locally.
+#' If `TRUE`, re-download all stickers.
+#'
+#' @returns `TRUE` if all stickers were downloaded successfully.
+#' Otherwise, `FALSE`.
+#' @export
+#'
+#' @examples
+#' if (interactive()) {
+#'   download_biocstickers()
+#' }
 download_biocstickers <- function(
-  url = biocstickers_readme_url,
-	where = "originals",
+	where = ".",
   overwrite = FALSE
 ) {
-	biocstickers_readme <- readLines(biocstickers_readme_url)
+  where <- file.path(where, .subdir_originals)
+	biocstickers_readme <- readLines(.biocstickers_readme_url)
 	png_urls <- .get_urls(biocstickers_readme)
 	dir.create(where, showWarnings = FALSE, recursive = TRUE)
 	.download_stickers(urls, where, overwrite)
+	return(TRUE)
 }
 
 ## get the full URL to each sticker
@@ -22,6 +37,7 @@ download_biocstickers <- function(
 }
 
 .download_stickers <- function(urls, where, overwrite) {
+  success <- TRUE
   for (url in urls) {
     filename <- basename(url)
     destfile <- file.path(where, filename)
@@ -31,9 +47,11 @@ download_biocstickers <- function(
         httr::GET(url, httr::write_disk(destfile, overwrite = overwrite))
       }, error = function(e) {
         message(paste("Failed to download", url, ":", e$message))
+        success <- FALSE
       })
     } else {
       # message(paste("Skipping", filename))
     }
   }
+  return(success)
 }
